1f05ff20011182209093ad5e92542c95ed56d4b0
Harry <sahilbhaisharma1212@gmail.com>
2025-04-24 23:38:10 +0530

    .

--- helpers/sendemail.ts
+++ helpers/sendemail.ts
@@ -3,7 +3,7 @@ import { ApiResponse } from "@/types/ApiResponse";
 
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function sendVerificationEmail({
   email,
